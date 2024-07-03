227a0043a26cb022bc81f637409850ad380370a6
5thAttemptCode <henrycodes@outlook.com>
2024-07-03 21:14:44 +0200

    upadted main page and added styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
+import CanvasComponent from "./components/canvas"
 import UserLoggedIn from "./components/userLoggedIn"
+import styles from './style.module.css'
 
 
 export default function Home() {
   
   return (
-    <UserLoggedIn />
+    <main>
+      <UserLoggedIn />
+      <section>
+        <h1 className={styles.h1}>The digital <span className={styles.span}>rubber duck</span> for developers</h1>
+        <p className={styles.p}>Every developer has the right to have a super cool rubber duck. 
+          With Duck Life you can create your personal rubber duck, 
+          for a better developing experience.
+        </p>
+        <a className={styles.cta} href="/login">Start now</a>
+      </section>
+      <CanvasComponent>
+        <ambientLight />
+      </CanvasComponent>
+    </main>
   )
 }
