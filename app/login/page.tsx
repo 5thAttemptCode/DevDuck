bd0892d644852556632470646bcff8aa60dfee2e
5thAttemptCode <henrycodes@outlook.com>
2024-07-05 11:37:13 +0200

    added styling

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,21 +1,26 @@
 import { login, signup, loginWithGitHub } from './actions'
+import styles from "./style.module.css"
 
 
 export default function LoginPage() {
 
   return (
-    <>
-      <form>
-        <label htmlFor="email">Email:</label>
-        <input id="email" name="email" type="email" required />
-        <label htmlFor="password">Password:</label>
-        <input id="password" name="password" type="password" required />
+    <section className={styles.section}>
+      <form className={styles.form} action={loginWithGitHub} method="post">
+        <button type="submit">Login with GitHub</button>
+      </form>
+      <form className={styles.form}>
+        <span className={styles.span}>
+          <label htmlFor="email">Email:</label>
+          <input id="email" name="email" type="email" required />
+        </span>
+        <span className={styles.span}>
+          <label htmlFor="password">Password:</label>
+          <input id="password" name="password" type="password" required />
+        </span>
         <button formAction={login}>Log in</button>
         <button formAction={signup}>Sign up</button>
-      </form>
-      <form action={loginWithGitHub} method="post">
-        <button type="submit">Login with GitHub</button>
-      </form>    
-    </>
+      </form> 
+    </section>
   )
 }
