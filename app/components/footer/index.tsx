b29c034624abd4f758cb3ff6471e89b09dd42f3a
5thAttemptCode <henrycodes@outlook.com>
2024-07-06 19:56:27 +0200

    new components

---
+++ app/components/footer/index.tsx
@@ -0,0 +1,13 @@
+import Logo from "../logo"
+import styles from "./style.module.css"
+
+
+const Footer = () => {
+  return (
+    <footer>
+      <Logo />
+    </footer>
+  )
+}
+ 
+export default Footer
---
+++ app/components/logo/index.tsx
@@ -0,0 +1,10 @@
+import styles from "./style.module.css"
+
+
+const Logo = () => {
+    return ( 
+        <a className={styles.logo} href="/">Duck Life</a>
+     )
+}
+ 
+export default Logo
