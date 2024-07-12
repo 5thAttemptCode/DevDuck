f40c953d78f4e1ea4a2ee825b460b3116ea5bf8f
5thAttemptCode <henrycodes@outlook.com>
2024-07-12 17:17:50 +0200

    cond. rendering and DropDown import

--- app/components/nav/index.tsx
+++ app/components/nav/index.tsx
@@ -1,12 +1,57 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { User } from '@supabase/supabase-js'
+import { createClient } from '@/utils/supabase/client'
+import DropdownMenu from '../dropdown'
 import Logo from '../logo'
 import UserLoggedIn from '../userLoggedIn'
 import styles from './style.module.css'
+import { UserCircle } from '@phosphor-icons/react'
+
+
+const supabase = createClient()
+
 
 export default function Nav() {
+  const [user, setUser] = useState<User | null>(null)
+
+  useEffect(() => {
+    async function getUser() {
+      const { data, error } = await supabase.auth.getUser()
+      if (error || !data?.user) {
+        setUser(null)
+      } else {
+        setUser(data.user)
+      }
+    }
+    getUser()
+
+    // Listen for auth changes
+    const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
+      if (event === 'SIGNED_OUT' || !session) {
+        setUser(null)
+      } else if (session) {
+        setUser(session.user)
+      }
+    })
+
+    // Cleanup listener on unmount
+    return () => {
+      authListener.subscription.unsubscribe()
+    }
+  }, [])
+
   return (
     <nav>
       <Logo />
-      <UserLoggedIn />
+      {!user ? (
+        <a className={styles.login} href="/login">Try Duck-Life</a>
+      ) : (
+        <DropdownMenu buttonContent={<UserCircle size={32} />}>
+          <UserLoggedIn />
+        </DropdownMenu>
+      )}
     </nav>
   )
 }
