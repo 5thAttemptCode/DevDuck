bffddec003c64f18a4e4e415da2440829955dbb1
5thAttemptCode <henrycodes@outlook.com>
2024-07-16 21:54:05 +0200

    updated PrivatePage with new MainScene

--- app/private/page.tsx
+++ app/private/page.tsx
@@ -1,15 +1,24 @@
 import { redirect } from 'next/navigation'
 import { createClient } from '@/utils/supabase/server'
+import CanvasComponent from '../components/canvas'
+import styles from "./style.module.css"
+import MainScene from '../components/canvas/components/mainscene'
 
 
 export default async function PrivatePage() {
   
   const supabase = createClient()
 
-  const { data, error } = await supabase.auth.getUser()
-  if (error || !data?.user) {
+  const { data: { user }, error } = await supabase.auth.getUser()
+  if (error || !user) {
     redirect('/login')
   }
 
-  return <p>Hello {data.user.email}</p>
+  return (
+    <main className={styles.main}>
+      <CanvasComponent>
+        <MainScene />
+      </CanvasComponent>
+    </main>
+  )
 }
