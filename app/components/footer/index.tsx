fc1c597723fb84ef87cfa6471bec68ad00f9e3ec
5thAttemptCode <henrycodes@outlook.com>
2024-07-28 23:49:27 +0200

    updated footer ui

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -5,7 +5,10 @@ import styles from "./style.module.css"
 const Footer = () => {
   return (
     <footer>
-      <Logo />
+      <div className={styles.footerContainer}>
+        <Logo />
+        <a href="https://www.linkedin.com/in/henry-fuerst-10b58a187/" target="_blank">LinkedIn</a>
+      </div>
     </footer>
   )
 }
