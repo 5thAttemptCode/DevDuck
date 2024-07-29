568a3d5e2b656badc75871a684c254c8aabba267
5thAttemptCode <henrycodes@outlook.com>
2024-07-29 15:49:37 +0200

    changed material props

--- app/components/canvas/components/model/duck/index.tsx
+++ app/components/canvas/components/model/duck/index.tsx
@@ -72,10 +72,12 @@ export function Duck(props: JSX.IntrinsicElements['group']) {
     }
   }, [])
 
+  const roughness = 0.6
+
   return (
     <group {...props} dispose={null}>
       <mesh geometry={nodes.Body.geometry} position={[0, 0.664, 0]} scale={[1, 0.643, 2.043]}>
-        <meshStandardMaterial color={bodyColor} />
+        <meshStandardMaterial color={bodyColor} roughness={roughness} />
       </mesh>
       <mesh geometry={nodes.BeekUpper.geometry} material={nodes.BeekUpper.material} position={[0, 1.948, 1.574]} scale={[0.823, 1, 0.788]}>
         <meshStandardMaterial color={beekColor} />
@@ -84,7 +86,7 @@ export function Duck(props: JSX.IntrinsicElements['group']) {
         <meshStandardMaterial color={beekColor} />
       </mesh>
       <mesh geometry={nodes.Wings.geometry} material={nodes.Wings.material} position={[0.018, 0.826, 0.193]} rotation={[0.18, 0, 0]} scale={[0.602, 0.387, 1.23]}>
-        <meshStandardMaterial color={bodyColor} />
+        <meshStandardMaterial color={bodyColor} roughness={roughness} />
       </mesh>
       <mesh geometry={nodes.Eyes.geometry} material={nodes.Eyes.material} position={[0.21, 2.216, 1.47]} rotation={[-0.289, 0.232, 0.068]} scale={[0.081, 0.101, 0.047]}>
         <meshStandardMaterial color={eyeColor} />
