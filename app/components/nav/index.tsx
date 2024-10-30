2b87574279f880a6d63cb3bfe014c4e164f5389e
5thAttemptCode <henrycodes@outlook.com>
2024-10-30 21:46:27 +1300

    imported SignedOut and SignedIn from Clerk

--- app/components/nav/index.tsx
+++ app/components/nav/index.tsx
@@ -1,25 +1,24 @@
 'use client'
 
-import DropdownMenu from '../dropdown'
 import Logo from '../logo'
-import UserLoggedIn from '../userLoggedIn'
 import styles from './style.module.css'
-import { UserCircle } from '@phosphor-icons/react'
-import { useUserContext } from '../../context/userContext'
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+
 
 export default function Nav() {
-  const { user, setUser } = useUserContext() // Use context to get user and setUser
 
   return (
     <nav>
       <Logo />
-      {!user ? (
-        <a className={styles.login} href="/login">Try DevDuck</a>
-      ) : (
-        <DropdownMenu buttonContent={<UserCircle size={32} />}>
-          <UserLoggedIn setUser={setUser} /> {/* Pass setUser to UserLoggedIn */}
-        </DropdownMenu>
-      )}
+      <ul className={styles.ul}>
+        <SignedOut>
+          <a className={styles.login} href="/login">Login</a>
+        </SignedOut>
+        <SignedIn>
+          <a className={styles.login} href="/private">Duck Editor</a>
+          <UserButton />
+        </SignedIn>
+      </ul>
     </nav>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,38 +2,47 @@
 
 import CanvasComponent from "./components/canvas"
 import Headline from "./components/headline"
-import { useUserContext } from "./context/userContext"
 import styles from './style.module.css'
+import { SignedIn, SignedOut } from "@clerk/nextjs"
 
 
 export default function Home() {
-
-  const { user } = useUserContext() 
   
   return (
     <main>
       <section>
         <h1 className={styles.h1}>The digital <span className={styles.span}>rubber duck</span> for developers</h1>
-        <p className={styles.p} style={{ textAlign: "center"}}>Every developer has the right to have a super cool rubber duck. 
+        <p className={styles.p} style={{ textAlign: "center"}}>
+          Every developer has the right to have a super cool rubber duck. 
           With DevDuck you can create your personal rubber duck, 
           for a better developing experience.
         </p>
-        <a className={styles.cta} href={!user ? "/login" : "/private"}>START NOW</a>
+        <SignedOut>
+          <a className={styles.cta} href="/">START NOW</a>
+        </SignedOut>
+        <SignedIn>
+          <a className={styles.cta} href="/private">Customize your Duck</a>
+        </SignedIn>
       </section>
+
       <CanvasComponent backgroundColor={undefined}>
         <ambientLight />
       </CanvasComponent>
+
       <section>
         <Headline text="Customize your own rubber duck" />
         <p className={styles.p}>
-          The reason I never got a physical rubber duck is because I hate their yellow color. My dream rubber duck is navy blue with a black beak.
-          I was sure I wasnt the only developer facing this issue, so DevDuck was born. Duck Life allows you to customize your own digital rubber duck.
+          The reason I never got a physical rubber duck is because I hate their yellow color. My dream rubber duck is navy 
+          blue with a black beak. I was sure I wasnt the only developer facing this issue, so DevDuck was born. DevDuck
+          allows you to customize your own digital rubber duck.
         </p>
       </section>
+      
       <section>
         <Headline text="Duck-off plastic!" />
         <p className={styles.p}>
-          Ain't nobody got time for plastic! Yes we could produce sustainable physical rubber ducks by reusing old plastic and blablabla, but 3D is just as cool.
+          Ain't nobody got time for plastic! Yes we could produce sustainable physical rubber ducks by reusing old 
+          plastic and blablabla, but 3D is just as cool.
         </p>
       </section>
     </main>
