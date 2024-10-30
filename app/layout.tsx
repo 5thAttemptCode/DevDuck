6f86a9670b37fea69ce50b8cdc38975c5d8d0c13
5thAttemptCode <henrycodes@outlook.com>
2024-10-30 21:42:16 +1300

    general update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,23 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Nav from "./components/nav";
-import Footer from "./components/footer";
-// import { UserProvider } from "./context/userContext";
-import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import { ClerkProvider } from "@clerk/nextjs"
 import { dark } from '@clerk/themes'
+import "./globals.css"
+import Nav from "./components/nav"
+import Footer from "./components/footer"
 
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "const quack = querySelector.DevDuck",
   description: "The coolest 3D rubber duck ever!",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
 
   return (
@@ -33,15 +32,13 @@ export default function RootLayout({
     >
     <html lang="en" suppressHydrationWarning={true}>
       <body className={inter.className}>
-        {/* <UserProvider> */}
         <header>
           <Nav />
         </header>
         {children}
         <Footer />
-        {/* </UserProvider> */}
       </body>
     </html>
     </ClerkProvider>
-  );
+  )
 }
