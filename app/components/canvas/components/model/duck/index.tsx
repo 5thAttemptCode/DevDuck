93efc63bbc693a135fb6df7bc6c8492e254dd5ca
5thAttemptCode <henrycodes@outlook.com>
2024-11-04 21:40:09 +1300

    cleanup - removed GuiComponent

--- app/components/canvas/components/model/duck/index.tsx
+++ app/components/canvas/components/model/duck/index.tsx
@@ -1,9 +1,8 @@
 import * as THREE from 'three'
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { useGLTF } from '@react-three/drei'
 import { GLTF } from 'three-stdlib'
-import { GuiComponent } from '../../gui'
-import { useThree } from '@react-three/fiber'
+import { useColor } from '@/context/colorContext'
 
 
 type GLTFAction = THREE.AnimationAction
@@ -24,87 +23,67 @@ type GLTFResult = GLTF & {
 export function Duck(props: JSX.IntrinsicElements['group']) {
 
   const { nodes } = useGLTF('/duck.gltf') as GLTFResult
-
-  const [ bodyColor, setBodyColor ] = useState(localStorage.getItem("bodyColor") || "#0000ff")
-  const [ beekColor, setBeekColor ] = useState(localStorage.getItem("beekColor") || "#ffa500")
-  const [ eyeColor, setEyeColor ] = useState(localStorage.getItem("eyeColor") || "#141414")
-  const backgroundColorValue = getComputedStyle(document.documentElement).getPropertyValue('--bg-clr-sec')
-  const [ backgroundColor, setBackgroundColor ] = useState(localStorage.getItem("backgroundColor") || backgroundColorValue)
-
+  const { colors } = useColor()
+  const { bodyColor, beekColor, eyeColor } = colors
   const roughness = 0.6
-
-  // Get the scene from the useFrame hook
-  const scene = useThree((state) => state.scene)
-
-  // Set the background color of the scene
-  useEffect(() => {
-    if (scene) {
-      scene.background = new THREE.Color(backgroundColor)
-    }
-  }, [scene, backgroundColor])
-
+ 
   return (
-    <>
-      <GuiComponent 
-        colorOptions={[
-          { folderName: "Body", colorKey: "bodyColor", defaultColor: bodyColor, setColor: setBodyColor },
-          { folderName: "Beek", colorKey: "beekColor", defaultColor: beekColor, setColor: setBeekColor },
-          { folderName: "Eyes", colorKey: "eyeColor", defaultColor: eyeColor, setColor: setEyeColor },
-          { folderName: "Background", colorKey: "backgroundColor", defaultColor: backgroundColor, setColor: setBackgroundColor },
-        ]}
-      />
-      <group {...props} dispose={null}>
-        <mesh 
-          geometry={nodes.Body.geometry} 
-          position={[0, 0.664, 0]} 
-          scale={[1, 0.643, 2.043]}
-        >
-          <meshStandardMaterial 
-            color={bodyColor} 
-            roughness={roughness} 
-          />
-        </mesh>
-        {/* upper and lower beek */}
-        <mesh 
-          geometry={nodes.BeekUpper.geometry} 
-          material={nodes.BeekUpper.material} 
-          position={[0, 1.948, 1.574]} 
-          scale={[0.823, 1, 0.788]}
-        >
-          <meshStandardMaterial color={beekColor} />
-        </mesh>
-        <mesh 
-          geometry={nodes.BeekLower.geometry} 
-          material={nodes.BeekLower.material} 
-          position={[0, 1.859, 1.549]} 
-          rotation={[0, 0, Math.PI]} 
-          scale={[0.76, 1, 0.788]}
-        >
-          <meshStandardMaterial color={beekColor} />
-        </mesh>
-        {/*_______________________*/}
-        <mesh 
-          geometry={nodes.Wings.geometry} 
-          material={nodes.Wings.material} 
-          position={[0.018, 0.826, 0.193]} 
-          rotation={[0.18, 0, 0]} 
-          scale={[0.602, 0.387, 1.23]}
-        >
-          <meshStandardMaterial 
-            color={bodyColor} 
-            roughness={roughness} 
-          />
-        </mesh>
-        <mesh 
-          geometry={nodes.Eyes.geometry} 
-          material={nodes.Eyes.material} 
-          position={[0.21, 2.216, 1.47]} 
-          rotation={[-0.289, 0.232, 0.068]} 
-          scale={[0.081, 0.101, 0.047]}
-        >
-          <meshStandardMaterial color={eyeColor} />
-        </mesh>
-      </group>
-    </>
+    <group {...props} dispose={null}>
+      {/* Body */}
+      <mesh 
+        geometry={nodes.Body.geometry} 
+        position={[0, 0.664, 0]} 
+        scale={[1, 0.643, 2.043]}
+      >
+        <meshStandardMaterial 
+          color={bodyColor} 
+          roughness={roughness} 
+        />
+      </mesh>
+
+      {/* upper and lower beek */}
+      <mesh 
+        geometry={nodes.BeekUpper.geometry} 
+        material={nodes.BeekUpper.material} 
+        position={[0, 1.948, 1.574]} 
+        scale={[0.823, 1, 0.788]}
+      >
+        <meshStandardMaterial color={beekColor} />
+      </mesh>
+      <mesh 
+        geometry={nodes.BeekLower.geometry} 
+        material={nodes.BeekLower.material} 
+        position={[0, 1.859, 1.549]} 
+        rotation={[0, 0, Math.PI]} 
+        scale={[0.76, 1, 0.788]}
+      >
+        <meshStandardMaterial color={beekColor} />
+      </mesh>
+
+      {/* Wings */}
+      <mesh 
+        geometry={nodes.Wings.geometry} 
+        material={nodes.Wings.material} 
+        position={[0.018, 0.826, 0.193]} 
+        rotation={[0.18, 0, 0]} 
+        scale={[0.602, 0.387, 1.23]}
+      >
+        <meshStandardMaterial 
+          color={bodyColor} 
+          roughness={roughness} 
+        />
+      </mesh>
+
+      {/* Eyes */}
+      <mesh 
+        geometry={nodes.Eyes.geometry} 
+        material={nodes.Eyes.material} 
+        position={[0.21, 2.216, 1.47]} 
+        rotation={[-0.289, 0.232, 0.068]} 
+        scale={[0.081, 0.101, 0.047]}
+      >
+        <meshStandardMaterial color={eyeColor} />
+      </mesh>
+    </group>
   )
 }
