fd16fedf55b7df971e1d8e7edd690da529a59128
5thAttemptCode <henrycodes@outlook.com>
2024-11-04 21:43:26 +1300

    ColorContext for GUI

--- app/components/canvas/components/gui/index.tsx
+++ app/components/canvas/components/gui/index.tsx
@@ -1,49 +1,33 @@
-"use client"
-
-import React, { useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import { GUI } from 'dat.gui'
+import { useColor } from '@/context/colorContext'
 
 
-interface GuiComponentProps {
-  colorOptions: Array<{
-    folderName: string
-    colorKey: string
-    defaultColor: string
-    setColor: React.Dispatch<React.SetStateAction<string>>
-  }>
-}
+export function GuiComponent() {
 
-
-export function GuiComponent({ colorOptions }: GuiComponentProps) {
-
+  const { colors, setColor } = useColor()
   const guiRef = useRef<GUI | null>(null)
 
   useEffect(() => {
-    // Create the GUI only once
     if (!guiRef.current) {
       const gui = new GUI({ autoPlace: false })
       gui.close()
       guiRef.current = gui
 
-      const targetElement = document.getElementById("canvas")
+      const targetElement = document.getElementById('canvas')
       if (targetElement) {
-        const customContainer = document.createElement("div")
-        customContainer.classList.add("gui")
-        targetElement.appendChild (customContainer)
+        const customContainer = document.createElement('div')
+        customContainer.classList.add('gui')
+        targetElement.appendChild(customContainer)
         guiRef.current.domElement.parentNode?.removeChild(guiRef.current.domElement)
         customContainer.appendChild(guiRef.current.domElement)
       }
 
-      // Add color options to the GUI
-      colorOptions.forEach((option) => {
-        const folder = gui.addFolder(option.folderName)
-        folder.addColor(option, 'defaultColor').onChange((value) => {
-          option.setColor(value)
-          localStorage.setItem(option.colorKey, value)
-        })
+      (Object.keys(colors) as Array<keyof typeof colors>).forEach((colorKey) => {
+        gui.addColor(colors, colorKey).onChange((value) => setColor(colorKey, value))
       })
     }
-  }, [colorOptions])
+  }, [ colors, setColor ])
 
-  return <></>
+  return null
 }
