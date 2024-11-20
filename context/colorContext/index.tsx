09119b21f5fbb149e8ca9605090bf787bc4602ae
5thAttemptCode <henrycodes@outlook.com>
2024-11-20 21:10:26 +1300

    removed localStorage due to deploy-error

--- context/colorContext/index.tsx
+++ context/colorContext/index.tsx
@@ -1,58 +1,47 @@
-"use client"
+"use client";
 
-import React, { createContext, useContext, useEffect, useState } from 'react'
-
+import React, { createContext, useContext, useState } from "react";
 
 interface Colors {
-  bodyColor: string
-  beekColor: string
-  eyeColor: string
-  backgroundColor: string
+  bodyColor: string;
+  beekColor: string;
+  eyeColor: string;
+  backgroundColor: string;
 }
 
 interface ColorContextType {
-  colors: Colors
-  setColor: (key: keyof Colors, value: string) => void
+  colors: Colors;
+  setColor: (key: keyof Colors, value: string) => void;
 }
 
+// Default color values
 const initialColors: Colors = {
-  bodyColor: localStorage.getItem("bodyColor") || "#010138",
-  beekColor: localStorage.getItem("beekColor") || "#000000",
-  eyeColor: localStorage.getItem("eyeColor") || "#ffffff",
-  backgroundColor: localStorage.getItem("backgroundColor") || "var(--bg-clr-sec)",
-}
+  bodyColor: "#010138",
+  beekColor: "#000000",
+  eyeColor: "#ffffff",
+  backgroundColor: "#23263a",
+};
 
-const ColorContext = createContext<ColorContextType | undefined>(undefined)
-
+const ColorContext = createContext<ColorContextType | undefined>(undefined);
 
 export const ColorProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-
-  const [ colors, setColors ] = useState<Colors>(initialColors)
-
-  // Save colors to localStorage when they change
-  useEffect(() => {
-    Object.entries(colors).forEach(([key, value]) => {
-      localStorage.setItem(key, value)
-    })
-  }, [ colors ])
+  const [colors, setColors] = useState<Colors>(initialColors);
 
   const setColor = (key: keyof Colors, value: string) => {
-    setColors((prevColors) => ({ ...prevColors, [key]: value }))
-  }
+    setColors((prevColors) => ({ ...prevColors, [key]: value }));
+  };
 
   return (
     <ColorContext.Provider value={{ colors, setColor }}>
       {children}
     </ColorContext.Provider>
-  )
-}
+  );
+};
 
 export const useColor = () => {
-
-  const context = useContext(ColorContext)
-
+  const context = useContext(ColorContext);
   if (!context) {
-    throw new Error("useColor must be used within a ColorProvider")
+    throw new Error("useColor must be used within a ColorProvider");
   }
-  return context
-}
+  return context;
+};
